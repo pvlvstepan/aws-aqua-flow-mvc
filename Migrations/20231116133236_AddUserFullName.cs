da3a637174b3be7e1b448a1d96e8f731315f22c9
pvlv.stepan <pvlv.stepan@STEPANPAVLO3B60>
2023-11-16 21:36:33 +0800

    feat: add new user model fields & cart view, controllers and model

---
+++ Migrations/20231116133236_AddUserFullName.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AquaFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 11, 16, 21, 32, 36, 863, DateTimeKind.Local).AddTicks(9073));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 11, 16, 21, 32, 36, 863, DateTimeKind.Local).AddTicks(9093));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 11, 16, 20, 52, 7, 433, DateTimeKind.Local).AddTicks(4013));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 11, 16, 20, 52, 7, 433, DateTimeKind.Local).AddTicks(4029));
+        }
+    }
+}
