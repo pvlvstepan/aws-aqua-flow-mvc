c3f9af6e5e94f3fee6c7c2818fa4a5e4ca7a3517
AHMED MOHAMED MOHAMED KAMAL KHALIL <ahmed.khalil.se@gmail.com>
2023-11-16 23:02:32 +0800

    add order and order item

--- Areas/Identity/Data/AquaFlowContext.cs
+++ Areas/Identity/Data/AquaFlowContext.cs
@@ -16,6 +16,8 @@ public class AquaFlowContext : IdentityDbContext<AquaFlowUser>
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
