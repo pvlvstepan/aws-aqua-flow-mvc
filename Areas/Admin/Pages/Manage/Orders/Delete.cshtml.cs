7c10601a5eeed99076ffcaa70832bc08c6f04b61
pvlv.stepan <pvlv.stepan@STEPANPAVLO3B60>
2023-11-17 20:11:01 +0800

    add details CRUD page

---
+++ Areas/Admin/Pages/Manage/Orders/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AquaFlow.Data;
+using AquaFlow.Models;
+
+namespace AquaFlow.Areas.Admin.Pages.Manage.Orders
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AquaFlow.Data.AquaFlowContext _context;
+
+        public DeleteModel(AquaFlow.Data.AquaFlowContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FirstOrDefaultAsync(m => m.OrderID == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Order = order;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order != null)
+            {
+                Order = order;
+                _context.Orders.Remove(Order);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Areas/Admin/Pages/Manage/Orders/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AquaFlow.Data;
+using AquaFlow.Models;
+
+namespace AquaFlow.Areas.Admin.Pages.Manage.Orders
+{
+    public class EditModel : PageModel
+    {
+        private readonly AquaFlow.Data.AquaFlowContext _context;
+
+        public EditModel(AquaFlow.Data.AquaFlowContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order =  await _context.Orders.FirstOrDefaultAsync(m => m.OrderID == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            Order = order;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.OrderID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Orders?.Any(e => e.OrderID == id)).GetValueOrDefault();
+        }
+    }
+}
